f2792f71380eb4b5144f74c66954efcafcb586a2
Paul Allan <paul@covet.studio>
2019-08-05 13:01:56 -0400

    new

---
+++ components/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+ <div>
+         <img class="colorTop" src="~assets/color.png" alt="IVY HOUSE">
+
+  <div class="icon">
+      <img src="~assets/ivy-house-icon.png" alt="IVY HOUSE">
+  </div>
+  <div class="logo">
+      <img src="~assets/ivy-house.png" alt="IVY HOUSE">
+  </div>
+   </div>
+</template>
+
+<style>
+.icon {
+  display: block;
+  position: relative;
+  overflow: hidden;
+  height: auto;
+  width: 105px;
+  margin: 80px auto 30px
+}
+.logo {
+  display: block;
+  position: relative;
+  overflow: hidden;
+  height: auto;
+  width: 405px;
+  margin: 40px auto 120px auto
+}
+.colorTop {
+  width: 100vw;
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+      <logo />
+
+
+<section class="section gg">
+
+<div class="container">
+
+<div class="columns">
+<div class="column has-text-white"> 
+
+<h2 class="subtitle" style="color:#D8C79E">OUR STORY</h2>
+<p class="is-6">
+Ivy House Recruiting is a Nashville-based recruiting firm that finds top-talent employees for teams all over the United States. With a background working for both large staffing companies and smaller boutique search firms, Ivy House Recruiting was founded when there seemed to be a lack of two things in the recruiting industry- integrity and kindness. Ivy House is in the people business. We create relationships by providing top-notch customer service founded on integrity and kindness. Striving to be the most professional, personable and efficient recruiting company you will ever partner with.
+
+Is your company ready to partner with a company that LOVES what they do? Are you looking for a new rockstar team member to join your team? Let Ivy House Recruiting, put our words into action and give us a call today. One quick call could take your team to the next level of success. 
+</p>
+
+</div> 
+
+<div class="column has-text-white"> 
+<h2 class="subtitle is-4" style="color:#D8C79E">OUR PROCESS</h2>
+<p class="is-4">
+Connecting the right person to the right company is a privilege that Ivy House Recruiting is passionate about. We are a company that truly gets to know our candidates before we know if they are an appropriate fit for you.  Ivy House is not in the business of sending our clients a ton of resumes for them to filter through, just to make our own metrics look good. We seek top of the line candidates that meet or exceed your expectations. Striving to find a mutual fit, so both parties can thrive and succeed together. Ivy House promises to only send you the very best. Through our pre-screening process we are able to determine if someone is going to fit the mold that your team specifically needs. Once approved, we remain there for both our clients, and our candidates. We make sure that interviews are professional, seamless, and well-coordinated. Every-step of the way Ivy House Recruiting is there for you.
+</p>
+
+</div> 
+
+</div>      
+
+  </div>
+</section>
+
+  <section class="section founder">
+
+    <div class="container">
+
+ <div class="columns">
+  <div class="column is-one-quarter">
+      <img class="" src="~assets/brit.jpg" alt="IVY HOUSE">
+  </div> 
+
+  <div class="column"> 
+<h2 class="subtitle">OUR FOUNDER<br/>Brittney Lapham</h2>
+ <p>
+Brittney Lapham started Ivy House Recruiting after working in professional recruiting and business development for over half a decade. First achieving her B.A. ata  Southeastern University in Lakeland Florida, she studied Public Relations and Journalism. Brittney then went abroad to Winchester, United Kingdom, to obtain her Masters in Managing Contemporary Global Issues. She is also the daughter of a United States Army Colonel/Assemblies of God Pastor and a Multi-tasking Superhero, who have taught her to live with honor and integrity. This upbringing also allowed her to live all over the world, furthering her foundation in servant leadership. Brittney has now settled in Nashville, Tennessee, where she has lived for 7 years. She is dedicated to her job, and her job is her passion. When she is not working on finding you the next perfect member for your team, you can find her (and her rescue dog Millie) on the hiking trails of beautiful Tennessee. 
+ </p>
+
+
+  </div> 
+
+</div>      
+</div>
+  </section>
+
+  <section class="section ilovepaul">
+    <div class="container">
+
+        <h2 class="subtitle has-text-centered ">INDUSTRIES</h2>
+
+<div class="columns is-vcentered has-text-centered is-gapless">
+
+  <div class="column">
+      Sales, Marketing & Corporate Services 
+  </div>
+<div class="column">
+Healthcare
+</div>
+<div class="column">
+Operations & Engineering
+</div>
+
+</div>
+<div class="columns is-vcentered has-text-centered">
+<div class="column">
+Automotive
+</div>
+<div class="column">
+Legal
+</div>
+<div class="column">
+Military Transition
+</div>
+</div>
+
+
+
+
+        </div>
+
+
+
+      </section> 
+  <section class="section contact is-medium has-text-white">
+        <h2 class="subtitle has-text-centered has-text-white"> CONTACT US </h2>
+        <h3 class="subtitle has-text-centered has-text-white">1 (615) 240-5577</h3>
+
+        </section>  
+    
+  <section class="section gg">
+      <logo />
+  </section>
+
+    </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+.founder {background: #F5F3EC}
+.gg {background: #21232B;}
+.gg .colorTop {display: none}
+.contact {background: #113F15}
+
+.is-gapless {padding-bottom: 0 !important}
+
+.columns {padding: 100px;}
+
+.title {
+  font-family: 'lato', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 600;
+  font-size: 30px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+*{font-family: "lato", sans-serif;}
+p {margin-bottom: 20px;font-family: "lato", sans-serif; font-weight: 400; font-size: 15px }
+.subtitle {
+  font-weight: 400;
+  font-size: 26px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-top: 15px;
+  padding-bottom: 0px;
+}
+
+
+</style>
