ecebbc29adb5d8095f319ca87e2443e7ddbe3809
Paul Allan <paul@covet.studio>
2019-08-06 11:14:08 -0400

    new title, fb image, copy edit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,12 +5,15 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'IVY HOUSE' || 'RECRUITING',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property: "og:image", content: "/ivy-fb.jpg"},
+      { property: "og:description", content: "Ivy House Recruiting is a Nashville-based recruiting firm that finds top-talent employees for teams all over the United States." },
+    { property: "og:url", content: "https://ivyhouserecruiting.com/"}
+  ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
